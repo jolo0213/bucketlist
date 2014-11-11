46e93db41daa9fa69907481dcd4a5ba5353f9e0e
jolo0213 <jca366@uchicago.edu>
2014-11-11 08:59:31 -0600

    Added item detail form parameters

--- blist/forms.py
+++ blist/forms.py
@@ -6,7 +6,7 @@ from blist.models import BL, Item
 class ItemForm(ModelForm):
 	class Meta:
 		model = Item
-		fields = ('item_value',)
+		fields = ('item_value','item_url','item_desc')
 
 class BLForm(ModelForm):
 	class Meta:
