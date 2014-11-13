69e49627fc8057accbfe287e40cfdab2523e1225
jolo0213 <jca366@uchicago.edu>
2014-11-13 14:33:32 -0600

    Reorder items on list page (doesn't save order) and removed Dajax

--- bucketlist/settings.py
+++ bucketlist/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'blist',
-    'dajaxice',
-    'dajax',
 )
 
 MIDDLEWARE_CLASSES = (
