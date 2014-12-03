4ae0f06cccca6d56d1eb9be4d0d564447e674e87
jolo0213 <jca366@uchicago.edu>
2014-12-03 12:55:31 -0600

    Added blist/contextprocessors.py to source control

---
+++ blist/contextprocessors.py
@@ -0,0 +1,8 @@
+import json
+
+from blist.models import Item
+
+def autocomplete(request):
+	ivals = json.dumps(list(Item.objects.filter(bucket__owner=request.user).values_list('item_value',flat=True)))
+	return { 'source':ivals }
+	
