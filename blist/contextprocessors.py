48fdd95eeced818c6a07f4f403b890d0910f6d0c
jolo0213 <jca366@uchicago.edu>
2014-12-11 10:27:12 -0600

    Test with full context processors

--- blist/contextprocessors.py
+++ blist/contextprocessors.py
@@ -5,13 +5,11 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def autocomplete(request):
-	"""
 	source = Item.objects.filter(bucket__owner=request.user).values('item_value')
 	ivals = []
 	for item in source:
 		ivals.append(item['item_value'])
 	ivals = json.dumps(ivals)
-	"""
 	# ivals = json.dumps(list(Item.objects.filter(bucket__owner=request.user).values_list('item_value',flat=True)))
 	return { 'source':ivals }
 	
