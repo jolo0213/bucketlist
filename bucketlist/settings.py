8515c6a4923c0ded4196260a6970a73d8bee54f0
jolo0213 <jca366@uchicago.edu>
2014-12-11 10:15:10 -0600

    Seeing if removing the context processor works

--- bucketlist/settings.py
+++ bucketlist/settings.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'bucketlist.wsgi.application'
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
     'django.contrib.auth.context_processors.auth',
-    'blist.contextprocessors.autocomplete',
+    # 'blist.contextprocessors.autocomplete',
 )
 
 # Database
