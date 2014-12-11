9ecaafe550a7f2da80512276de43437ad85f5863
jolo0213 <jca366@uchicago.edu>
2014-12-11 09:59:59 -0600

    Fixing contextprocessor error

--- blist/contextprocessors.py
+++ blist/contextprocessors.py
@@ -1,7 +1,9 @@
 import json
 
 from blist.models import Item
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def autocomplete(request):
 	ivals = json.dumps(list(Item.objects.filter(bucket__owner=request.user).values_list('item_value',flat=True)))
 	return { 'source':ivals }
