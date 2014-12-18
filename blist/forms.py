6d67a8e93dd0798d5e55277ccb82eee68054deb0
jolo0213 <jca366@uchicago.edu>
2014-12-18 13:06:25 -0600

    Styled forms

--- blist/forms.py
+++ blist/forms.py
@@ -7,15 +7,37 @@ class ItemForm(ModelForm):
 	class Meta:
 		model = Item
 		fields = ('item_value','item_url','item_desc')
-		widgets = {'item_desc': Textarea(attrs={'cols': 35, 'rows': 7}),}
+		widgets = {
+			'item_value':forms.TextInput(attrs={
+				'placeholder':'Item Name',
+				'class':'form-control',
+				}),
+			'item_desc': Textarea(attrs={
+				'placeholder':'Description (optional)',
+				'class':'form-control',
+				}),
+			'item_url':forms.TextInput(attrs={
+				'placeholder':'URL (optional)',
+				'class':'form-control',
+				}),
+			}
 
 class BLForm(ModelForm):
 	class Meta:
 		model = BL
 		fields = ('name',)
+		widgets = {
+			'name': forms.TextInput(attrs={
+				'placeholder':'List Name','class':'form-control'
+				}),
+			}
 
 class SharedForm(ModelForm):
 	class Meta:
 		model = SharedList
 		fields = ('name',)
-		
+		widgets = {
+			'name': forms.TextInput(attrs={
+				'placeholder':'Username','class':'form-control'
+				}),
+			}
