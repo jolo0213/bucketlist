e27467ed5e7dbd9d6ca70fb0d744eacabc2346ca
jolo0213 <jca366@uchicago.edu>
2014-12-19 08:44:36 -0600

    Fixed context processor for anonymous

--- blist/contextprocessors.py
+++ blist/contextprocessors.py
@@ -1,15 +1,15 @@
 import json
 
 from blist.models import Item
-from django.contrib.auth.decorators import login_required
 
-@login_required
 def autocomplete(request):
-	# source = Item.objects.filter(bucket__owner=request.user).values('item_value')
-	# ivals = []
-	# for item in source:
-	# 	ivals.append(item['item_value'])
-	# ivals = json.dumps(ivals)
-	ivals = json.dumps(list(Item.objects.filter(bucket__owner=request.user).values_list('item_value',flat=True)))
-	return { 'source':ivals }
-	
+	if request.user.is_authenticated():
+		# source = Item.objects.filter(bucket__owner=request.user).values('item_value')
+		# ivals = []
+		# for item in source:
+		# 	ivals.append(item['item_value'])
+		# ivals = json.dumps(ivals)
+		ivals = json.dumps(list(Item.objects.filter(bucket__owner=request.user).values_list('item_value',flat=True)))
+		return { 'source':ivals }
+	else:
+		return { 'source':[] }
